f450c92c5115aed3a820bb6bd72e147728ed26d8
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2024-10-18 00:46:43 -0700

    added home page route

--- scalable_annotation_service/frontend/src/App.js
+++ scalable_annotation_service/frontend/src/App.js
@@ -1,13 +1,17 @@
-// src/App.js
 import React from 'react';
-import Login from './components/Login'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home'; 
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Login /> 
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
--- scalable_annotation_service/frontend/src/components/Login.js
+++ scalable_annotation_service/frontend/src/components/Login.js
@@ -1,10 +1,16 @@
-// src/components/Login.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    navigate('/home'); 
+  };
+
   return (
     <div className="login-container">
       <h1>Marken</h1>
@@ -14,7 +20,7 @@ const Login = () => {
           <span className="button-text">Sign Up</span>
         </button>
         <div className="separator" />
-        <button className="login-button signin-button">
+        <button className="login-button signin-button" onClick={handleSignInClick}>
           <FontAwesomeIcon icon={faGithub} className="icon" />
           <span className="button-text">Sign In</span>
         </button>
